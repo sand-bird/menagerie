b071ce7348250990903c8a8f6cd270eef2170f32
sand-bird <sandbird@whimsy.fun>
2017-11-30 06:45:02 -0500

    moving trait notes into the code was a dumb idea tbh

--- game/monster/traits.gd
+++ game/monster/traits.gd
@@ -51,18 +51,6 @@ class Trait:
 
 
 class Iq extends Trait:
-# - highly hereditary
-# - while pet is young, can be improved by increasing its learning; 
-#   potential for improvement decreases rapidly as pet ages
-# ---
-# - strongly contributes to INT
-# - affects how much discipline applies to past actions
-# - affects how likely the pet is to undertake complicated (multipart) actions, 
-#   like shaking a tree for food (how to implement this???)
-# - affects how quickly the pet learns skills (like what?)
-# - (maybe) affects how much discipline applies to certain types of actions, like training?
-# - causes pet to bond slightly faster with other high-INT pets (affects preference increase)
-# - causes pet to prefer "INT-type" items and foods (affects pref. increase) (is this gonna be a thing?)
 	func _init():
 		name = "iq"
 		max_value = 200
@@ -77,12 +65,6 @@ class Iq extends Trait:
 
 
 class Learning extends Trait:
-# LEARNING - worldly, naive
-# - expose the pet to as many different kinds of things as possible
-# - increase pet's skills
-# - starts at 0
-# ---
-# - ?
 	func _init():
 		name = "learning"
 		max_value = 100
@@ -104,14 +86,6 @@ class Learning extends Trait:
 
 
 class Size extends Trait:
-# - improve: keep pet consistently well fed (small to medium impact)
-# - inherit from parent
-# - size is relative for species, not absolute
-# ---
-# - mostly for flavor, contributes slightly to VIT (if at all!!)
-# - highly hereditary (makes offspring more likely to be bigger)
-# - makes the pet more intimidating, maybe other pets try to fight with it less
-# - makes pet require slightly more food (food fills less belly, or belly cap is increased)
 	func _init():
 		name = "size"
 		max_value = 100
@@ -123,13 +97,6 @@ class Size extends Trait:
 
 
 class Strength extends Trait:
-# STRENGTH - brawny, wimpy
-# - starting value somewhere between 0 and middle (maybe depends on base size)
-# - maybe slight dependence on species (adjust when pet evolves)
-# ---
-# - makes pet more likely to win fights
-# - reduces energy drain & increases energy cap (maybe)
-# - affects pet's willingness to accept a fight vs run away
 	func _init():
 		name = "strength"
 		max_value = 100
@@ -141,13 +108,6 @@ class Strength extends Trait:
 
 
 class Health extends Trait:
-# HEALTH (or "heartiness") - hearty, sickly/frail
-# ---
-# - affects lifespan
-# - affects pet's chance of getting sick
-# - affects fertility (?)
-# - increases energy recovery rate and energy cap
-# - affects pet's willingness to accept a fight vs run away
 	func _init():
 		name = "health"
 		max_value = 100
@@ -165,11 +125,6 @@ class Health extends Trait:
 
 
 class Composure extends Trait:
-# - ?
-# ---
-# - when pet's mood DECREASES, triggers a PROC that wil lessen the decrease. 
-#   this is so that composure can be trained by praising after a successful proc. 
-#   (proc comes with an emote that is different from the regular mood hit emote)
 	func _init():
 		name = "composure"
 		max_value = 100
@@ -181,12 +136,6 @@ class Composure extends Trait:
 
 
 class Willpower extends Trait:
-# - ?
-# ---
-# - duration of activities that drain energy, response to low energy
-# - affects the priority pet places on energy-recovering actions (ENERGY DRIVE PRIORITY FORMULA)
-# - affects pet's likelihood to quit current action due to low energy (threshold for "quit" event)
-# - two options for training: come up with some way to make this a proc'd action so we can train it, or make it increase passively whenever pet is forced to the limits of its energy
 	func _init():
 		name = "willpower"
 		max_value = 100
@@ -198,12 +147,6 @@ class Willpower extends Trait:
 
 
 class Patience extends Trait:
-# - ?
-# ---
-# - affects the lower threshold for pet's status before it considers its needs unmet?
-# - affects the length of time the pet will wait with unmet needs before it starts taking mood hits
-# - HOW TO TRAIN THIS?? possibly a proc on above? (as is, pet will emote hunger before it's hungry enough to affect mood. really just need to work out how mood should respond to unmet drives ("meter management")
-# - factor in MOOD DRIVE PRIORITY FORMULA
 	func _init():
 		name = "patience"
 		max_value = 100
@@ -221,12 +164,6 @@ class Patience extends Trait:
 
 
 class Confidence extends Trait:
-# - mostly rng, slightly dependent on pet's sociability
-# - improve by praising social actions (esp when of pet's own will)
-# ---
-# - increases pet's willingness to socialize (too low and it will not meet its own social needs)
-# - includes willingness to follow orders to socialize)
-# - slightly increases other pets' preference gain for this pet (maybe half of beauty)
 	func _init():
 		name = "confidence"
 		max_value = 100
@@ -238,12 +175,6 @@ class Confidence extends Trait:
 
 
 class Beauty extends Trait:
-# - mostly hereditary, small amount of rng at birth
-# - can be slightly improved by good grooming (TODO: how does grooming work) and consistently high health & energy
-# - really good candidate for a potion or spell (maybe damage some other trait a random amt)
-# ---
-# - increases the preference gain of other pets toward your pet
-# - contributes a lot to CHA
 	func _init():
 		name = "beauty"
 		max_value = 100
@@ -255,11 +186,6 @@ class Beauty extends Trait:
 
 
 class Poise extends Trait:
-# - tbh this is a pretty CON-centric trait, maybe have it depend on similar factors??
-# ---
-# - affects how much mood influences social actions (or actions in general, like complaining)
-# - TODO: figure out how mood is supposed to influence social actions in the first place (this is important)
-# - affects how much other pets' behavior influence mood (eg less mood hit from insults or fights)
 	func _init():
 		name = "poise"
 		max_value = 100
@@ -277,10 +203,6 @@ class Poise extends Trait:
 
 
 class Independence extends Trait:
-# - 60% rng, 40% hereditary
-# - decreases a tiny amount with every discipline action
-# ---
-# - affects how likely pet is to obey orders (esp with low loyalty)
 	func _init():
 		name = "independence"
 		max_value = 100
@@ -292,13 +214,6 @@ class Independence extends Trait:
 
 
 class Arrogance extends Trait:
-# - positive correlation with confidence and iq, maybe roll on birth depending on those
-# ---
-# - contributes negatively to AMI
-# - promotes discriminatory behavior against pets with very different traits/preferences
-# - increases chance to reject acts of kindness from other pets (scolding this will lower arrogance)
-# - strongly decreases preference gain toward other pets whose attributes are lower, or who have many preferences opposite this pet's preferences
-# - decrease other pets' preference gain for this pet (especially from other arrogant pets(?))
 	func _init():
 		name = "arrogance"
 		max_value = 100
@@ -310,12 +225,6 @@ class Arrogance extends Trait:
 
 
 class Aggressiveness extends Trait:
-# AGGRESSIVENESS - bellicose, zen/placid/quiescent
-# ---
-# - likelihood to fight when choosing a social action
-# - contributes a lot to AMI (negatively)
-# - scold when pet picks a fight to reduce aggressiveness (also praise on peaceful social interaction reduces it slightly)
-# - affects pet's willingness to accept a fight vs run away
 	func _init():
 		name = "aggressiveness"
 		max_value = 100
@@ -327,8 +236,6 @@ class Aggressiveness extends Trait:
 
 
 class Empathy extends Trait:
-# ---
-# - affects pet's mood to be more like the moods of pets around it (this should not cause a preference hit to those pets though, probably)
 	func _init():
 		name = "empathy"
 		max_value = 100
@@ -340,9 +247,6 @@ class Empathy extends Trait:
 
 
 class Kindness extends Trait:
-# ---
-# - prioritizes "nice" interactions (play, cheer up) over "mean" ones (fight, insult)
-# - TODO: enumerate interaction types and triggers
 	func _init():
 		name = "kindness"
 		max_value = 100
@@ -360,13 +264,6 @@ class Kindness extends Trait:
 
 
 class Happiness extends Trait:
-# - represents pet's average mood
-# - calculate once per day: @ time of birth, roll n % 24 for hour of update that day (where "day" starts & ends at time of birth)
-# - fold day's calculation into lifetime average
-# - maybe just save the total mood (yes it will ALWAYS fit in an integer) - that way total mood contributes some, and average mood contributes some, plus makes average easier to calculate
-# ---
-# - does nothing except contribute to SPR
-# - strength of effect has to be carefully balanced; maybe should depend on age as well as value
 	func _init():
 		name = "happiness"
 		max_value = 0
@@ -383,10 +280,6 @@ class Happiness extends Trait:
 
 
 class Actualization extends Trait:
-# - represents improvement across traits
-# - starts at 0, and increases by a certain amount each time a trait increases (1 for most, 2 for spiritually important traits like confidence, health, learning, all the CON skills, 0 for irrelevant traits like beauty and size, -1 for arrogance/aggression)
-# ---
-# - does nothing except contribute to SPR
 	func _init():
 		name = "actualization"
 		max_value = 0
@@ -399,12 +292,6 @@ class Actualization extends Trait:
 
 
 class Loyalty extends Trait:
-# - represents relationship with player (basically pet's preference toward player)
-# - improved by causing mood increases in pet and maybe other stuff (see discipline.md)
-# ---
-# - shows up on pet loadout as 0-10 (?) hearts
-# - makes pet more likely to follow player orders
-# - small effect on SPR
 	func _init():
 		name = "loyalty"
 		max_value = 10
@@ -423,10 +310,6 @@ class Loyalty extends Trait:
 
 
 class Appetite extends Trait:
-# - ?
-# ---
-# - factor in BELLY DRIVE PRIORITY FORMULA
-# - makes it easier to gain priority toward foods
 	func _init():
 		name = "appetite"
 		max_value = 100
@@ -437,10 +320,6 @@ class Appetite extends Trait:
 
 
 class Openness extends Trait:
-# - ?
-# ---
-# - modulates target selection by preference (makes pet more likely to choose targets with less preference) (TARGET PRIORITY FORMULA?)
-# - makes it easier to gain preference (esp toward targets pet is not naturally disposed to)?
 	func _init():
 		name = "openness"
 		max_value = 100
@@ -451,12 +330,6 @@ class Openness extends Trait:
 
 
 class Sociability extends Trait:
-# - ?
-# ---
-# - reflects pet's preference for socializing 
-# - affects rate of increase/decrease of social meter & social cap (maybe)
-# - factor in SOCIAL DRIVE PRIORITY FORMULA
-# - influences the effect on mood of max/min social (with low sociability, too much interaction will decrease mood)
 	func _init():
 		name = "sociability"
 		max_value = 100
