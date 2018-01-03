dda0a1198d52ce2e1ae217617f41d00eb04db770
sand-bird <sandbird@whimsy.fun>
2018-01-03 00:59:05 -0500

    fading attempt 2 (didn't work)

--- game/system/menu.gd
+++ game/system/menu.gd
@@ -6,8 +6,12 @@ onready var ui = get_tree().get_root().get_node("game/ui")
 var menu_stack = []
 
 func open(menu):
-	if menu_stack.find_last(menu) != -1: return
+	print(menu_stack)
+	if menu_stack.has(menu): 
+		print("MENU ALREADY OPEN! ", menu)
+		return
 	menu_stack.append(menu)
+	print("open: ", menu_stack)
 	ui.open_menu(menu)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().set_pause(true)
@@ -19,9 +23,11 @@ func change(menu):
 func close():
 	if menu_stack.empty(): return false
 	var menu = menu_stack.back()
-	print("entering menu_close")
+	print("close: ", menu_stack)
+#	print("entering menu_close")
 	ui.close_menu(menu)
-	print("exiting menu_close")
+	yield(ui, "closed")
+#	print("exiting menu_close")
 	menu_stack.pop_back()
 	if menu_stack.empty(): get_tree().set_pause(false)
 	return true
--- game/ui/ui.gd
+++ game/ui/ui.gd
@@ -1,26 +1,42 @@
 extends CanvasLayer
 
-onready var fader = get_node("fader")
+signal closed
+
+#onready var fader = get_node("fader")
+func print_children():
+	var children = ""
+	for child in get_children():
+		children += child.get_name() + ", "
+	print(children)
+	
 
 func open_menu(menu):
-	var anim = fader.get_animation("fade_in")	
+#	var anim = fader.get_animation("fade_in")
 	load_menu(menu)
+	print_children()
 	get_node(menu).set_focus_mode(Control.FOCUS_ALL)
 	get_node(menu).grab_focus()
 
 func close_menu(menu):
 	# trying to save the node in menu stack to pass it
 	# in here is not working, so let's just do this
-	print("before fade out")
-	fader.fade_out(menu, 1.5)
+#	print("before fade out")
+#	fader.fade_out(menu, 1.5)
+#	yield(fader, "finished")
+#	print("after fade out")
+	var fader = get_node(menu + "/fader")
+	fader.play("fade_out")
 	yield(fader, "finished")
-	print("after fade out")
 	remove_child(get_node(menu))
+	emit_signal("closed", menu)
 #	remove_child(get_children().back())
 
 func load_menu(menu):
-	add_child(load("res://ui/" + menu + ".tscn").instance())
-	print("before fade in")
-	fader.fade_in(menu, 0.8)
-	yield(fader, "finished")
-	print("after fade in")
+	var new_node = load("res://ui/" + menu + ".tscn").instance()
+	new_node.add_child(load("res://ui/fader.tscn").instance())
+	add_child(new_node)
+#	add_child(new_node)
+#	print("before fade in")
+#	fader.fade_in(menu, 0.8)
+#	yield(fader, "finished")
+#	print("after fade in")
