1f853f620077a9670219c37150742c73e850e70e
sand-bird <sandbird@whimsy.fun>
2019-03-15 17:00:05 -0400

    fix some dumb ui stuff

--- game/ui/arrows.gd
+++ game/ui/arrows.gd
@@ -9,32 +9,13 @@ func _ready():
 	$left.connect("pressed", self, "emit_signal", ["change_page", -1])
 	$right.connect("pressed", self, "emit_signal", ["change_page", 1])
 
+func restart_animations():
+	$left/sprite/anim.stop()
+	$right/sprite/anim.stop()
+	$left/sprite/anim.play(ANIM)
+	$right/sprite/anim.play(ANIM)
+
 func update_visibility(current, total):
-	if current > 0:
-		$left.show()
-		$left/sprite/anim.play(ANIM)
-	else:
-		$left.hide()
-		$left/sprite/anim.stop()
-
-	if current < total - 1:
-		$right.show()
-		$right/sprite/anim.play(ANIM)
-	else:
-		$right.hide()
-		$right/sprite/anim.stop()
-
-func update_viz2(current, total):
-	if current == 0 and $left.visible:
-		$left.hide()
-		$left/sprite/anim.stop()
-	elif !$left.visible:
-		$left/sprite/anim.play(ANIM)
-		$left.show()
-
-	if current >= total - 1 and $right.visible:
-		$right.hide()
-		$right/sprite/anim.stop()
-	elif !$right.visible:
-		$right/sprite/anim.play(ANIM)
-		$right.show()
+	$left.visible = current > 0
+	$right.visible = current < total - 1
+	restart_animations()
