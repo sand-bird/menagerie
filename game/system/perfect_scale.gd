0ac9e55f04bed38333bcc292ff033251db6bbbab
sand bird <sandbird@whimsy.fun>
2019-03-21 18:30:26 -0400

    slightly increase min height, improve PerfectScale comments

--- game/system/perfect_scale.gd
+++ game/system/perfect_scale.gd
@@ -8,18 +8,19 @@ extends Node
 const IDEAL_SIZE = Vector2(460, 320)
 # TOFIX: screen clipping in fullscreen when min sizes
 # are inconsistent with the aspect ratio constraints
-const MIN_SIZE = Vector2(320, 196)
+const MIN_SIZE = Vector2(320, 208)
 const MAX_SIZE = Vector2(480, 360)
 
-const MIN_ASPECT = 0.5 # 9.0/16.0
-const MAX_ASPECT = 0.75 # 3.0/4.0
+# superceded by the maximum and minimum size constraints
+const MIN_ASPECT = 9.0/19.5
+const MAX_ASPECT = 3.0/4.0
 
-const MIN_SCALE = 2
+const MIN_SCALE = 1
 
 # DEBOUNCE_TIME represents how long we wait for resize
 # signals to stop coming before we do our thing, while the
 # timer keeps track of this for us.
-const DEBOUNCE_TIME = 0.2
+const DEBOUNCE_TIME = 0.1
 var timer: SceneTreeTimer = null
 
 func _ready():
@@ -52,11 +53,13 @@ func update_screen():
 	timer = null
 
 	var viewport = get_tree().get_root()
+	var win_size = OS.window_size
 
-	var new_win_size = OS.window_size
-	var new_scale = get_scale(new_win_size)
-	var base_size = get_new_size(new_win_size, new_scale)
+	var new_scale = get_scale(win_size)
+	var base_size = get_new_size(win_size, new_scale)
 	var scaled_size = base_size * new_scale
+
+	viewport.set_size(base_size)
 
 	if OS.is_window_maximized() or OS.is_window_fullscreen():
 		var gutter = ((OS.window_size - scaled_size) / 2).floor()
@@ -64,17 +67,37 @@ func update_screen():
 	else:
 		OS.window_size = scaled_size
 
-	viewport.set_size(base_size)
-
-	Log.debug(self, ["base size: ", base_size, " | scaled size: ",
-			scaled_size, " | window size: ", OS.window_size])
+	Log.debug(self, ["window size: ", OS.window_size,
+			" | scaled size: ", scaled_size,
+			" | base size: ", base_size, " | viewport size: ",
+			viewport.size, " | scale: ", new_scale])
 
 	return base_size
 
 # -----------------------------------------------------------
 
+# the scale is found using IDEAL_SIZE, then clamped to the
+# minimum and maximum possible scale just in case. if the
+# window's width exceeds the maximum aspect ratio, we must
+# calculate the scale using its height.
+func get_scale(win_size):
+	var i = 'y' if win_size.y / win_size.x < MIN_ASPECT else 'x'
+	return max(
+		clamp(
+			round(win_size[i] / IDEAL_SIZE[i]),
+			ceil(win_size[i] / MAX_SIZE[i]),
+			floor(win_size[i] / MIN_SIZE[i])
+		),
+		MIN_SCALE
+	)
+
+# -----------------------------------------------------------
+
 # sets `base_size` according to the new window dimensions and
-# our constraints. also sets `scale` through get_scale().
+# our constraints. if the window is wider than the minimum
+# aspect ratio, we must calculate base_size from the window's
+# height; same with width if it's taller than the maximum.
+# here we use width by default, and height when necessary.
 func get_new_size(win_size, scale):
 	var new_x
 	var new_y
@@ -89,28 +112,6 @@ func get_new_size(win_size, scale):
 	return Vector2(round(new_x), round(new_y))
 
 # -----------------------------------------------------------
-
-# the scale is found using IDEAL_SIZE, then clamped to
-# the minimum and maximum possible scale just in case.
-func get_scale(win_size):
-	var i = 'y' if win_size.y / win_size.x < MIN_ASPECT else 'x'
-	return max(
-		clamp(
-			round(win_size[i] / IDEAL_SIZE[i]),
-			ceil(win_size[i] / MAX_SIZE[i]),
-			floor(win_size[i] / MIN_SIZE[i])
-		),
-		MIN_SCALE
-	)
-
-# -----------------------------------------------------------
-
-# getting primary and secondary dimensions
-# -----------------------------------------
-# if the window is wider than the minimum aspect ratio, we
-# must calculate base_size from the window's height; same
-# with width if it's taller than the maximum. here we use
-# width by default, and height when necessary.
 
 func get_primary(i, win_size, scale):
 	return max(win_size[i] / scale,
